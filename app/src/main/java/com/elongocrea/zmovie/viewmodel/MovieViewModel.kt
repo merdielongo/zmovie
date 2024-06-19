f07dbfab9f76fac7032864889eb20932d42ff749
merdielongo <merdielongo9@gmail.com>
2024-06-19 12:44:04 +0100

    [Add] ViewModel - ajout du viewModel pour les movies

--- app/src/main/java/com/elongocrea/zmovie/viewmodel/MovieViewModel.kt
+++ app/src/main/java/com/elongocrea/zmovie/viewmodel/MovieViewModel.kt
@@ -1,2 +1,35 @@
 package com.elongocrea.zmovie.viewmodel
 
+import androidx.lifecycle.ViewModel
+import com.elongocrea.zmovie.data.local.model.Movie
+import com.elongocrea.zmovie.data.repository.MovieRepository
+import io.reactivex.rxjava3.core.Flowable
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+
+class MovieViewModel(private val repository: MovieRepository) : ViewModel() {
+    private val disposables = CompositeDisposable()
+
+    val all: Flowable<List<Movie>> = repository.all
+
+    fun create(movie: Movie) {
+        repository.create(movie)
+    }
+
+    fun delete(movie: Movie) {
+        repository.delete(movie)
+    }
+
+    fun deleteAll() {
+        repository.deleteAll()
+    }
+
+    fun fetchMoviesFromApi(): Single<List<Movie>> {
+        return repository.fetchListFromApi()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposables.clear()
+    }
+}
