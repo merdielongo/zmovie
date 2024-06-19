df8545f24f3e158cde67d02749f627ea4cd94e28
merdielongo <merdielongo9@gmail.com>
2024-06-19 11:35:17 +0100

    [DAO] Movie - ajout du DAO pour les films

--- app/src/main/java/com/elongocrea/zmovie/data/local/dao/MovieDao.kt
+++ app/src/main/java/com/elongocrea/zmovie/data/local/dao/MovieDao.kt
@@ -1,4 +1,29 @@
 package com.elongocrea.zmovie.data.local.dao
 
-class MovieDao {
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.elongocrea.zmovie.data.local.model.Movie
+import io.reactivex.rxjava3.core.Flowable
+
+@Dao
+interface MovieDao {
+
+    @Insert
+    fun insert(movie: Movie)
+
+    @Query("SELECT * FROM movies")
+    fun getAll(): Flowable<List<Movie>>
+
+    @Delete
+    fun delete(movie: Movie)
+
+    @Query("DELETE FROM movies")
+    fun deleteAll()
+
+    @Update
+    fun update(movie: Movie)
+
 }
