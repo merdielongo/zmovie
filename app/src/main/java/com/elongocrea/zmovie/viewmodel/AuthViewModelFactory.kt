4d896b5f16bb0040a78fad3898b116a01f5e5d3d
merdielongo <merdielongo9@gmail.com>
2024-06-19 12:47:50 +0100

    Fix: ViewModelFactory

--- app/src/main/java/com/elongocrea/zmovie/viewmodel/AuthViewModelFactory.kt
+++ app/src/main/java/com/elongocrea/zmovie/viewmodel/AuthViewModelFactory.kt
@@ -1,4 +1,15 @@
 package com.elongocrea.zmovie.viewmodel
 
-class AuthViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.elongocrea.zmovie.auth.FirebaseAuthManager
+
+class AuthViewModelFactory(private val authManager: FirebaseAuthManager) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(AuthViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return AuthViewModel(authManager) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
--- app/src/main/java/com/elongocrea/zmovie/viewmodel/MovieViewModelFactory.kt
+++ app/src/main/java/com/elongocrea/zmovie/viewmodel/MovieViewModelFactory.kt
@@ -1,4 +1,15 @@
 package com.elongocrea.zmovie.viewmodel
 
-class MovieViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.elongocrea.zmovie.data.repository.MovieRepository
+
+class MovieViewModelFactory(private val repository: MovieRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MovieViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return MovieViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
