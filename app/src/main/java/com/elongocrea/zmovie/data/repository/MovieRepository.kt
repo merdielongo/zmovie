ab0daa91153a29cd679a9c8cef10cca10b68bf00
merdielongo <merdielongo9@gmail.com>
2024-06-19 12:09:29 +0100

    [Add] Repository - ajout de l'implementation pour MovieRepository

--- app/src/main/java/com/elongocrea/zmovie/data/repository/MovieRepository.kt
+++ app/src/main/java/com/elongocrea/zmovie/data/repository/MovieRepository.kt
@@ -1,4 +1,33 @@
 package com.elongocrea.zmovie.data.repository
 
-class MovieRepository {
+import com.elongocrea.zmovie.data.local.dao.MovieDao
+import com.elongocrea.zmovie.data.local.model.Movie
+import com.elongocrea.zmovie.data.remote.api.TheMovieDbApi
+import io.reactivex.rxjava3.core.Flowable
+import io.reactivex.rxjava3.core.Single
+
+class MovieRepository(
+    private val movieDao: MovieDao,
+    private val api: TheMovieDbApi,
+    private val apiKey: String
+) {
+
+    val all: Flowable<List<Movie>> = movieDao.getAll()
+
+    fun create(movie: Movie) {
+        movieDao.insert(movie)
+    }
+
+    fun delete(movie: Movie) {
+        movieDao.delete(movie)
+    }
+
+    fun deleteAll() {
+        movieDao.deleteAll()
+    }
+
+    fun fetchListFromApi(): Single<List<Movie>> {
+        return api.getListPopular(apiKey).map { it.results }
+    }
+
 }
