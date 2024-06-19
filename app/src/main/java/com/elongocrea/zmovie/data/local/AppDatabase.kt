f41d37b36b138bc7c7611c3a21603c9b5e112616
merdielongo <merdielongo9@gmail.com>
2024-06-19 11:38:48 +0100

    [DAO] RoomDatabase - configuration de la base de donnee

--- app/src/main/java/com/elongocrea/zmovie/data/local/AppDatabase.kt
+++ app/src/main/java/com/elongocrea/zmovie/data/local/AppDatabase.kt
@@ -1,4 +1,11 @@
 package com.elongocrea.zmovie.data.local
 
-class AppDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.elongocrea.zmovie.data.local.dao.MovieDao
+import com.elongocrea.zmovie.data.local.model.Movie
+
+@Database(entities = [Movie::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun movieDao(): MovieDao
 }
