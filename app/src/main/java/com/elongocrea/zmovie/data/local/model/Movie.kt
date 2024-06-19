7d79cf5b65acaadfae04adadbeb0837a75ee764f
merdielongo <merdielongo9@gmail.com>
2024-06-19 11:30:47 +0100

    [Model] Movie - ajout du model des les films

--- app/src/main/java/com/elongocrea/zmovie/data/local/model/Movie.kt
+++ app/src/main/java/com/elongocrea/zmovie/data/local/model/Movie.kt
@@ -1,4 +1,16 @@
 package com.elongocrea.zmovie.data.local.model
 
-class Movie {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "movies")
+data class Movie(
+    @PrimaryKey(autoGenerate = false) val id: Int,
+    @ColumnInfo(name = "web_id")
+    val webId: Int,
+    val title: String,
+    val overview: String,
+    val releaseDate: String,
+    val posterPath: String?
+)
